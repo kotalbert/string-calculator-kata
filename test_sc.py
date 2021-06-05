dcb163afd6a591bc083d7dbb5cc481edf5c5b72b
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-06-05 20:41:50 +0200

    RED: single digit should be converted to value

--- test_sc.py
+++ test_sc.py
@@ -8,6 +8,11 @@ class TestStringCalculator:
         """Empty string should produce 0"""
         assert sc.add('') == 0
 
+    @pytest.mark.parametrize('test_input, expected', [('1', 1), ('2', 2)])
+    def test_single_digit(self, test_input, expected, sc):
+        """Single digit should be converted to value"""
+        assert sc.add(test_input) == expected
+
 
 @pytest.fixture()
 def sc():
