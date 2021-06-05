383a9c1e2111d2e3590ef17b41085b21707c1d39
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-06-05 21:11:40 +0200

    BLUE: Extract operations to methods

--- sc.py
+++ sc.py
@@ -5,11 +5,20 @@ class StringCalculator:
     def add(self, input_str: str) -> int:
         if input_str == '':
             return 0
-        s = 0
-        for number_str in input_str.split(','):
-            s += self._string_to_number(number_str)
-        return s
+        vals = input_str.split(',')
+        return self._sum_vals(vals)
 
     @staticmethod
     def _string_to_number(value: str) -> int:
         return int(value)
+
+    @staticmethod
+    def _sum_vals(vals: List[str]) -> int:
+        s = 0
+        for v in vals:
+            s += StringCalculator._string_to_number(v)
+        return s
+
+    @staticmethod
+    def _split_str(s: str) -> List[str]:
+        return s.split(',')
