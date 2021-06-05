cf6f2c48f897d0a2fd126e8edbe92b3a9c941902
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-06-05 22:35:55 +0200

    RED: Value separator should be possible to set

--- test_sc.py
+++ test_sc.py
@@ -26,7 +26,10 @@ class TestStringCalculator:
     def test_either_separated_values(self, sc):
         assert sc.add('1\n2,3\n4') == 10
 
-    @pytest.mark.parametrize('test_input, expected', [('-1,2,-3', '-1,-3'), ('-1,2', '-1'), ('1,-2', '-2')])
+    @pytest.mark.parametrize('test_input, expected',
+                             [('-1,2,-3', '-1,-3'),
+                              ('-1,2', '-1'),
+                              ('1,-2', '-2')])
     def test_negative_values_not_allowed(self, test_input, expected, sc):
         """Negatives in input should raise exception"""
         expected_err_msg = f'negatives not allowed {expected}'
@@ -38,10 +41,19 @@ class TestStringCalculator:
         except ValueError as e:
             assert str(e) == expected_err_msg
 
-    @pytest.mark.parametrize('test_input, expected', [('999,1000', 1999),
-                                                      ('1,999,1000,1001', 2000),
-                                                      ('1001', 0)])
+    @pytest.mark.parametrize('test_input, expected',
+                             [('999,1000', 1999),
+                              ('1,999,1000,1001', 2000),
+                              ('1001', 0)])
     def test_gt_1000_ignored(self, test_input, expected, sc):
+        """Numbers > 1000 should be ignored."""
+        assert sc.add(test_input) == expected
+
+    @pytest.mark.parametrize('test_input, expected',
+                             [('//#\n1#2', 3),
+                              ])
+    def test_separator_should_be_possible_to_set(self, test_input, expected, sc):
+        """Value separator should be possible to set."""
         assert sc.add(test_input) == expected
 
 
